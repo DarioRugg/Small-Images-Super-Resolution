c8ff78154c291345f27b2594fa02dc37f2641856
rom42pla <romeo.lanzino@gmail.com>
2020-12-01 19:02:32 +0100

    Working RRDB

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from models import Classifier, RRDB, test_model
+from models import Classifier, RRDB, test_model, show_img
 
-assets_path = "./assets"
+assets_path = f"./assets"
+logs_path = f"{assets_path}/logs"
 imagenet2012_path = f"{assets_path}/ImageNet2012"
 rrdb_pretrained_weights_path = f"{assets_path}/models/RRDB_PSNR_x4.pth"
 
@@ -12,19 +14,20 @@ imagenet2012_dataset = datasets.ImageFolder(root=imagenet2012_path,
                                                 transforms.Resize(256),
                                                 transforms.RandomCrop(224),
                                                 transforms.ToTensor(),
-                                                transforms.Normalize(
-                                                    mean=[0.485, 0.456, 0.406],
-                                                    std=[0.229, 0.224, 0.225])
+                                                #transforms.Normalize(
+                                                 #   mean=[0.485, 0.456, 0.406],
+                                                  #  std=[0.229, 0.224, 0.225])
                                             ]))
 imagenet2012_loader = DataLoader(imagenet2012_dataset,
-                                 batch_size=1, shuffle=True, num_workers=4)
+                                 batch_size=2, shuffle=True, num_workers=4)
 
 #classifier = Classifier()
-#test_model(model=classifier, dataloader=imagenet2012_loader)
+#test_model(model=classifier, data=imagenet2012_loader)
 
 rrdb = RRDB(pretrained_weights_path=rrdb_pretrained_weights_path)
-for batch in imagenet2012_loader:
-    X = batch[0].to("cuda")
-    print(X.shape)
-    output = rrdb(X).data.squeeze().float().cpu().clamp_(0, 1).numpy()
-    print(output)
+with torch.no_grad():
+    for batch in imagenet2012_loader:
+        X = batch[0].to("cuda")
+        output = rrdb(X).data
+        show_img(X[0], output[0], save_to_folder=logs_path)
+        break
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import time
+from typing import Union
 
 import numpy as np
 import pandas as pd
@@ -8,12 +9,18 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import models
+from torchvision.utils import save_image
 
 import RRDBNet_arch as arch
 
+
 class Classifier(nn.Module):
     def __init__(self, device: str = "auto"):
+        # checks that the device is correctly given
         assert device in {"cpu", "cuda", "auto"}
+        self.device = device if device in {"cpu", "cuda"} else \
+            "cuda" if torch.cuda.is_available() else "cpu"
+
         super(Classifier, self).__init__()
 
         self.resnet18 = models.resnet18(pretrained=True)
@@ -22,22 +29,23 @@ class Classifier(nn.Module):
             self.resnet18
         )
 
-        # passes the model to the selected device
-        if device == "auto":
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.device = device
+        # moves the entire model to the chosen device
         self.to(self.device)
-
-        self.eval()
 
     def forward(self, X: torch.Tensor):
         out = self.layers(X)
         return out
 
+
 class RRDB(nn.Module):
     def __init__(self, pretrained_weights_path: str, device: str = "auto"):
+        # checks that the device is correctly given
         assert device in {"cpu", "cuda", "auto"}
+        self.device = device if device in {"cpu", "cuda"} else \
+            "cuda" if torch.cuda.is_available() else "cpu"
+        # checks that the weights are correctly given
         assert isinstance(pretrained_weights_path, str)
+
         super(RRDB, self).__init__()
 
         self.rrdb = arch.RRDBNet(3, 3, 64, 23, gc=32)
@@ -47,52 +55,55 @@ class RRDB(nn.Module):
             self.rrdb
         )
 
-        # passes the model to the selected device
-        if device == "auto":
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.device = device
+        # moves the entire model to the chosen device
         self.to(self.device)
-
-        self.eval()
 
     def forward(self, X: torch.Tensor):
         out = self.layers(X)
         return out
 
-def test_model(model: nn.Module, dataloader: DataLoader, verbose: bool = True):
+
+def test_model(model: nn.Module, data: DataLoader, verbose: bool = True):
     assert isinstance(model, nn.Module)
-    assert isinstance(dataloader, DataLoader)
+    assert isinstance(data, DataLoader)
     assert isinstance(verbose, bool)
 
     loss_function = nn.CrossEntropyLoss()
-    losses, times, starting_time = np.zeros(shape=len(dataloader)), \
-                                   np.zeros(shape=len(dataloader)), \
+    losses, times, starting_time = np.zeros(shape=len(data)), \
+                                   np.zeros(shape=len(data)), \
                                    time.time()
-    for i_batch, batch in enumerate(dataloader):
-        # plot a sample image if it's the first time
-        if i_batch == 0 and verbose:
-            show_img(batch[0][0])
-        batch_starting_time = time.time()
-        # make a prediction
-        X, y = batch[0].to(model.device), batch[1].to(model.device)
-        y_pred = model(X)
-        losses[i_batch], times[i_batch] = loss_function(y_pred, y), \
-                                          time.time() - batch_starting_time
-        # prints some stats
-        if i_batch != 0 and i_batch % (len(dataloader) / 20) == 0 and verbose:
-            print(pd.DataFrame(index=[i_batch], data={
-                "avg loss": [np.mean(losses[:i_batch])],
-                "avg time per batch (s)": [np.mean(times[:i_batch])],
-                "total elapsed time (s)": [time.time() - starting_time]
-            }))
+    with torch.no_grad():
+        for i_batch, batch in enumerate(data):
+            # plot a sample image if it's the first time
+            if i_batch == 0 and verbose:
+                show_img(batch[0][0])
+            batch_starting_time = time.time()
+            # make a prediction
+            X, y = batch[0].to(model.device), batch[1].to(model.device)
+            y_pred = model(X)
+            losses[i_batch], times[i_batch] = loss_function(y_pred, y), \
+                                              time.time() - batch_starting_time
+            # prints some stats
+            if i_batch != 0 and i_batch % (len(data) / 20) == 0 and verbose:
+                print(pd.DataFrame(index=[f"batch {i_batch} of {len(data)}"], data={
+                    "avg loss": [np.mean(losses[:i_batch])],
+                    "avg time per batch (s)": [np.mean(times[:i_batch])],
+                    "total elapsed time (s)": [time.time() - starting_time]
+                }))
 
     return np.mean(losses)
 
-def show_img(img: torch.Tensor):
-    # reshapes the tensor to make it viewable
-    img = img.permute(1, 2, 0)
-    # prints the image
-    plt.imshow(img.numpy())
-    plt.axis('off')
+
+def show_img(*imgs: torch.Tensor, save_to_folder: str = None):
+    assert not save_to_folder or isinstance(save_to_folder, str)
+    imgs = list(imgs)
+    for i_img, img in enumerate(imgs):
+        assert isinstance(img, torch.Tensor)
+        assert len(img.shape) == 3
+        if save_to_folder:
+            save_image(img, f"{save_to_folder}/img{i_img}_{int(time.time())}.png")
+        imgs[i_img] = img.permute(1, 2, 0).to("cpu").numpy()
+    fig, axs = plt.subplots(1, len(imgs), squeeze=False)
+    for i_ax, ax in enumerate(axs.flat):
+        ax.imshow(imgs[i_ax])
     plt.show()
-    print(f"Image of shape {tuple(img.shape)}")
