3e1804d888722674b5c2265b65e73625236c433c
Simonerc95 <s.ercolino995@gmail.com>
2021-02-04 15:15:25 +0100

    train with lightning

--- blocks.py
+++ blocks.py
@@ -1,21 +1,62 @@
 from typing import Union
-
+from os.path import join
+import matplotlib.pyplot as plt
 import torch
 from torch import nn
 import torch.nn.functional as F
 from torchvision import models, transforms
-from PIL.Image import BICUBIC, NEAREST, BILINEAR
+from torchvision.utils import save_image
+from PIL import Image
 import RRDBNet_arch as arch
 import pytorch_lightning as pl
-from positional_encodings import PositionalEncodingPermute2D
 import numpy as np
 import pandas as pd
+import math
 
 def psnr(img1, img2):
     mse = torch.mean((img1 - img2) ** 2)
     if mse == 0:
         return np.inf
     return 20 * torch.log10(1 / torch.sqrt(mse))
+
+def show_img(*imgs: torch.Tensor, filename: str = None, save_to_folder: str = None):
+    assert not save_to_folder or isinstance(save_to_folder, str)
+    imgs = list(imgs)[0]
+    for i_img, img in enumerate(imgs):
+        assert isinstance(img, torch.Tensor)
+        assert len(img.shape) == 3
+        if save_to_folder:
+            filename = filename if filename else f"img_{i_img}"
+            save_image(img, join(save_to_folder, f"{filename}.png"))
+        imgs[i_img] = img.permute(1, 2, 0).to("cpu").numpy()
+    fig, axs = plt.subplots(1, len(imgs), squeeze=False)
+    for i_ax, ax in enumerate(axs.flat):
+        ax.imshow(imgs[i_ax])
+    plt.show()
+
+def positionalencoding2d(d_model, tensor):
+    """
+    :param d_model: dimension of the model
+    :param height: height of the positions
+    :param width: width of the positions
+    :return: d_model*height*width position matrix
+    """
+    if d_model % 4 != 0:
+        raise ValueError("Cannot use sin/cos positional encoding with "
+                         "odd dimension (got dim={:d})".format(d_model))
+    height, width = tensor.shape[2:]
+    pe = torch.zeros(d_model, height, width, device=tensor.device)
+    # Each dimension use half of d_model
+    d_model = int(d_model / 2)
+    div_term = torch.exp(torch.arange(0., d_model, 2, device=tensor.device) *
+                         -(math.log(10000.0) / d_model))
+    pos_w = torch.arange(0., width, device=tensor.device).unsqueeze(1)
+    pos_h = torch.arange(0., height, device=tensor.device).unsqueeze(1)
+    pe[0:d_model:2, :, :] = torch.sin(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    pe[1:d_model:2, :, :] = torch.cos(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    pe[d_model::2, :, :] = torch.sin(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+    pe[d_model + 1::2, :, :] = torch.cos(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+    return pe
 
 class Classifier(nn.Module):
     def __init__(self, device: str = "auto"):
@@ -38,9 +79,9 @@ class Classifier(nn.Module):
         self.to(self.device)
 
     def forward(self, X: torch.Tensor):
-        X_normalized = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        X = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                             std=[0.229, 0.224, 0.225])(X)
-        out = self.layers(X_normalized)
+        out = self.layers(X)
         return out
 
 
@@ -66,7 +107,7 @@ class Scaler(nn.Module):
 
 class RRDB(pl.LightningModule):
     def __init__(self, pretrained_weights_path: str = '0', trainable: bool = False, device: str = "auto",
-                 nb: int = 23):
+                 nb: int = 23, lr: float = 1e-4):
         # checks that the device is correctly given
         assert device in {"cpu", "cuda", "auto"}
         self.device_str = device if device in {"cpu", "cuda"} else \
@@ -92,21 +133,23 @@ class RRDB(pl.LightningModule):
         # moves the entire model to the chosen device
         self.epoch = 0
         self.batch_size = None
-        self.lr = 1e-4
+        self.lr = lr
         self.to(self.device_str)
         self._ce = nn.CrossEntropyLoss
-        self._l1 = nn.L1Loss
+        self._mse = nn.MSELoss
         self._resnet = Classifier()
         for par in self._resnet.parameters():
             par.requires_grad = False
         self._epoch_losses, self._epoch_psnrs , self._epoch_pred_loss, self._epoch_CrossEntropy = 0,0,0,0
         self._epoch_val_losses, self._epoch_val_psnrs, self._epoch_val_pred_loss, self._epoch_val_CrossEntropy = 0,0,0,0
+        self._best_loss = np.inf
         self._counter = 0
         self._counter_val = 0
         self.outputs = []
+        self.handles = []
         self.stats = pd.DataFrame(columns=["epoch", "phase", "avg loss", "avg PSNR", "avg prediction loss", "avg CE"])
     def hook(self, module, input, output):
-        self.outputs.append(output.detach())
+        self.outputs.append(output)
 
 
     def configure_optimizers(
@@ -121,39 +164,45 @@ class RRDB(pl.LightningModule):
         if not self.batch_size:
             self.batch_size = X.shape[0]
         X_downsampled = Scaler(int(X.shape[-1] * 0.32))(X)
-        self.train()
-        X_supersampled = torch.clip(transforms.Resize(256)(self(X_downsampled)), 0, 1)
+        with torch.cuda.amp.autocast():
+            self.train()
+            X_supersampled = transforms.Resize(256)(self(X_downsampled))
 
-        self.outputs = []
-        self.handles = []
-        for i in range(3):
-            self.handles.append(self._resnet.layers[0].layer1[i].conv1.register_forward_hook(self.hook))
-            self.handles.append(self._resnet.layers[0].layer1[i].conv2.register_forward_hook(self.hook))
-        for i in range(4):
-            self.handles.append(self._resnet.layers[0].layer2[i].conv1.register_forward_hook(self.hook))
-            self.handles.append(self._resnet.layers[0].layer2[i].conv2.register_forward_hook(self.hook))
+            for i in range(3):
 
-        gt_pred = self._resnet(X)
+                self.handles.append(self._resnet.layers[0].layer1[i].conv3.register_forward_hook(self.hook))
+            for i in range(4):
 
-        bl_ce = self._ce()(gt_pred, y)
-        y_pred = self._resnet(X_supersampled)
+                self.handles.append(self._resnet.layers[0].layer2[i].conv3.register_forward_hook(self.hook))
 
-        posenc_loss = 0
-        for i in range(len(self.outputs) // 2):
-            pos_enc = PositionalEncodingPermute2D(int(self.outputs[i].shape[1]))(self.outputs[i])
-            posenc_loss += self._l1()(pos_enc + self.outputs[i], pos_enc + self.outputs[i + len(self.outputs) // 2])
-        for handle in self.handles:
-            handle.remove()
-        CE = self._ce()(y_pred, y)
-        pred_loss = self._l1()(y_pred, gt_pred)
+            self.handles.append(self._resnet.layers[0].layer3[0].conv2.register_forward_hook(self.hook))
+            self.handles.append(self._resnet.layers[0].layer4[0].conv2.register_forward_hook(self.hook))
 
-        loss = pred_loss + posenc_loss
-        self._epoch_losses += loss.item()
-        self._epoch_psnrs += psnr(X, X_supersampled).item()
+            gt_pred = self._resnet(X)
 
-        self._epoch_pred_loss += pred_loss.item()
-        self._epoch_CrossEntropy += (CE - bl_ce).item()
-        self._counter += 1
+            bl_ce = self._ce()(gt_pred, y)
+            y_pred = self._resnet(X_supersampled)
+
+            percep_loss = 0
+            for i in range(len(self.outputs) // 2):
+                percep_loss += self._mse()(
+                    self.outputs[i], self.outputs[i + len(self.outputs) // 2])
+            percep_loss /= len(self.outputs) // 2
+            for handle in self.handles:
+                handle.remove()
+            self.outputs = []
+            self.handles = []
+
+            CE = self._ce()(y_pred, y)
+            pred_loss = self._mse()(y_pred, gt_pred)
+
+            loss = pred_loss + percep_loss + self._mse()(X_supersampled, X)
+            self._epoch_losses += loss.item()
+            self._epoch_psnrs += psnr(X, X_supersampled).item()
+
+            self._epoch_pred_loss += pred_loss.item()
+            self._epoch_CrossEntropy += (CE - bl_ce).item()
+            self._counter += 1
 
         return loss
 
@@ -161,40 +210,48 @@ class RRDB(pl.LightningModule):
 
         X, y = val_batch
         X_downsampled = Scaler(int(X.shape[-1] * 0.32))(X)
-        self.eval()
-        X_supersampled = torch.clip(transforms.Resize(256)(self(X_downsampled)), 0, 1)
 
-        self.outputs = []
-        self.handles = []
-        for i in range(3):
-            self.handles.append(self._resnet.layers[0].layer1[i].conv1.register_forward_hook(self.hook))
-            self.handles.append(self._resnet.layers[0].layer1[i].conv2.register_forward_hook(self.hook))
-        for i in range(4):
-            self.handles.append(self._resnet.layers[0].layer2[i].conv1.register_forward_hook(self.hook))
-            self.handles.append(self._resnet.layers[0].layer2[i].conv2.register_forward_hook(self.hook))
+        with torch.cuda.amp.autocast():
+            self.eval()
+            X_supersampled = transforms.Resize(256)(self(X_downsampled))
 
-        gt_pred = self._resnet(X)
+            for i in range(3):
+                self.handles.append(self._resnet.layers[0].layer1[i].conv1.register_forward_hook(self.hook))
+                self.handles.append(self._resnet.layers[0].layer1[i].conv2.register_forward_hook(self.hook))
+                self.handles.append(self._resnet.layers[0].layer1[i].conv3.register_forward_hook(self.hook))
+            for i in range(4):
+                self.handles.append(self._resnet.layers[0].layer2[i].conv1.register_forward_hook(self.hook))
+                self.handles.append(self._resnet.layers[0].layer2[i].conv2.register_forward_hook(self.hook))
+                self.handles.append(self._resnet.layers[0].layer2[i].conv3.register_forward_hook(self.hook))
 
-        bl_ce = self._ce()(gt_pred, y)
-        y_pred = self._resnet(X_supersampled)
+            self.handles.append(self._resnet.layers[0].layer3[0].conv2.register_forward_hook(self.hook))
+            self.handles.append(self._resnet.layers[0].layer4[0].conv2.register_forward_hook(self.hook))
 
-        posenc_loss = 0
-        for i in range(len(self.outputs) // 2):
-            pos_enc = PositionalEncodingPermute2D(512)(self.outputs[i])
-            posenc_loss += self._l1()(pos_enc + self.outputs[i], pos_enc + self.outputs[i + len(self.outputs) // 2])
-        for handle in self.handles:
-            handle.remove()
+            gt_pred = self._resnet(X)
 
-        CE = self._ce()(y_pred, y)
-        pred_loss = self._l1()(y_pred, gt_pred)
+            bl_ce = self._ce()(gt_pred, y)
+            y_pred = self._resnet(X_supersampled)
 
-        loss = pred_loss + posenc_loss
-        self._epoch_val_losses+= loss.item()
-        self._epoch_val_psnrs+= psnr(X, X_supersampled).item()
+            percep_loss = 0
+            for i in range(len(self.outputs) // 2):
+                percep_loss += self._mse()(
+                     self.outputs[i], self.outputs[i + len(self.outputs) // 2])
+            percep_loss /= len(self.outputs)//2
+            for handle in self.handles:
+                handle.remove()
+            self.outputs = []
+            self.handles = []
 
-        self._epoch_val_pred_loss+= pred_loss.item()
-        self._epoch_val_CrossEntropy += (CE - bl_ce).item()
-        self._counter_val += 1
+            CE = self._ce()(y_pred, y)
+            pred_loss = self._mse()(y_pred, gt_pred)
+
+            loss = pred_loss + percep_loss + self._mse()(X_supersampled, X)
+            self._epoch_val_losses+= loss.item()
+            self._epoch_val_psnrs+= psnr(X, X_supersampled).item()
+
+            self._epoch_val_pred_loss+= pred_loss.item()
+            self._epoch_val_CrossEntropy += (CE - bl_ce).item()
+            self._counter_val += 1
 
     def on_epoch_end(self):
 
@@ -207,11 +264,27 @@ class RRDB(pl.LightningModule):
                                            "avg CE":[np.round(self._epoch_CrossEntropy/(self._counter), 3),np.round(self._epoch_val_CrossEntropy/(self._counter_val), 3)]}))
             print(self.stats.to_string(index=False))
             print()
+        val_loss = self._epoch_val_losses/self._counter_val
+        if  val_loss < self._best_loss:
+            print(f"Found Best Model With Loss: {val_loss}")
+            self.cpu()
+            torch.save(self.layers.state_dict(), join('assets', 'models', 'Darionet_percep.pth'))
+            self.to(self.device_str)
+            self._best_loss = val_loss
+
         self._epoch_losses, self._epoch_psnrs, self._epoch_pred_loss, self._epoch_CrossEntropy = 0, 0, 0, 0
         self._epoch_val_losses, self._epoch_val_psnrs, self._epoch_val_pred_loss, self._epoch_val_CrossEntropy = 0, 0, 0, 0
         self._counter = 0
         self._counter_val = 0
         self.epoch += 1
+        goldfish = join('assets', 'sample_images', 'pesce_rosso.jpg')
+        colosseum = join('assets', 'sample_images', 'colosseo.png')
+        imgs = [Image.open(goldfish), Image.open(colosseum)]
+        imgs = [transforms.ToTensor()(transforms.CenterCrop(256)(transforms.Resize(256)(X))).to('cuda') for X in imgs]
+        X_downsampled = [Scaler(int(X.shape[-1] * 0.32))(X) for X in imgs]
+        X_upsampled = [self(X.unsqueeze(dim=0))[0].detach() for X in X_downsampled]
+        show_img(X_downsampled)
+        show_img(X_upsampled)
 
     def forward(self, X: torch.Tensor):
         out = self.layers(X)
--- train_darionet.py
+++ train_darionet.py
@@ -32,7 +32,7 @@ if not resume_from:
         os.mkdir(check_path)
 else:
     check_path = resume_from
-rrdb_pretrained_weights_path, DarioNet_pretrained_model_path = join(models_path, "RRDB_PSNR_x4.pth"), \
+rrdb_pretrained_weights_path, DarioNet_pretrained_model_path = join(models_path, "RRDB_ESRGAN_x4.pth"), \
                                                                join(models_path, "DarioNet_fromzero.pt")
 
 transform_train = transforms.Compose([
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     #                scale=parameters['transformations']['scale'], train_crop_size=parameters['transformations']['train_crop_size'],
     #                val_crop_size=parameters['transformations']['val_crop_size'], save=parameters['training']['save'],
     #                checkpoints=check_path)
-    trainer = pl.Trainer(gpus=1, precision=16, num_sanity_val_steps=4, max_epochs=parameters['training']['epochs'], benchmark=True)
+    trainer = pl.Trainer(gpus=1, num_sanity_val_steps=4, max_epochs=parameters['training']['epochs'],
+                         benchmark=True)
     trainer.tune(darionet, imagenet2012_train_loader, imagenet2012_val_loader)
     trainer.fit(darionet, imagenet2012_train_loader, imagenet2012_val_loader)
